5ad0aa6d7a5a198fa01f8d6e9944f8f9fed52c72
Sofiya Polyanska <sofiya@spiretrading.com>
2022-05-03 17:16:27 -0400

    Added a dummy column that slides along the table like a sliding door.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,56 @@
 import React from 'react';
 import {SlidingTable} from './sliding_table'
 import './App.css';
+import { stat } from 'fs';
 
 interface State {
-  currentIndex: number | null;
-  headerOrder: number[]
+  slidingColumn: number | null;
+  headerOrder: number[];
+  mouseStart: number;
+  mouseCurrent: number;
 }
 
 class App extends React.Component<{}, State> {
     constructor(props: {}) {
     super(props);
     this.state = {
-      currentIndex: null,
-      headerOrder: [4, 0, 1, 2, 3]
+      slidingColumn: null,
+      headerOrder: [4, 0, 1, 2, 3],
+      mouseStart: 0,
+      mouseCurrent: 0,
     };
   }
 
   public render(): JSX.Element {
     return (
       <div className="App">
-        <header className="App-header">
-          <SlidingTable
-            headerOrder={this.state.headerOrder}
-            header={headers}
-            data={data}
-            onMouseDown={(index) => {console.log(index)}}
-            onMouseUp={() => {console.log('index cleared')}}/>
-        </header>
+        <SlidingTable
+          headerOrder={this.state.headerOrder}
+          header={headers}
+          data={data}
+          slidingColumnXPosition={this.state.mouseCurrent - this.state.mouseStart}
+          slidingColumn={this.state.slidingColumn}
+          onMouseDown={this.onMouseDown}
+          onMouseUp={() => {this.setState({slidingColumn: null})}}
+          onMouseMove={this.onMouseMove}/>
       </div>
     );
+  }
+
+  private onMouseDown = (x: number, col: number) => {
+    console.log(x, col);
+    this.setState({
+      mouseCurrent: x,
+      mouseStart: x,
+      slidingColumn: col
+    });
+  }
+
+  private onMouseMove = (newCurrent: number) => {
+    console.log(newCurrent - this.state.mouseStart)
+    this.setState({
+      mouseCurrent: newCurrent
+    });
   }
 }
 
@@ -40,5 +62,7 @@ const data = [
   ['Green', 'Instant', 'Giant Growth', '0.5', 'Yes'],
   ['White', 'Instant', 'Steppe Lynx', '03.00', 'Yes']
 ]
+
+const column_width = 150;
 
 export default App;
--- app/src/sliding_table.tsx
+++ app/src/sliding_table.tsx
@@ -5,8 +5,12 @@ interface Properties {
   headerOrder: number[];
   header: string[];
   data: string[][];
-  onMouseDown: (index: number) => void;
+  slidingColumn: number | null;
+  //Can be negative, indicating it has slid off the table, or positive.
+  slidingColumnXPosition: number;
+  onMouseDown: (x: number, index: number) => void;
   onMouseUp: () => void;
+  onMouseMove: (newX: number) => void;
 }
 
 export class SlidingTable extends React.Component<Properties> {
@@ -16,46 +20,122 @@ export class SlidingTable extends React.Component<Properties> {
 
   public render(): JSX.Element {
     return (
-      <table>
-        <thead>
-          <tr>
-            {this.props.headerOrder.map(this.renderHeader)}
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.data.map(this.renderRow)}
-        </tbody>
-      </table>
+      <div className='rails'>
+        <SlidingColumn  show={this.props.slidingColumn !== null}
+          leftValue={this.props.slidingColumnXPosition}/>
+        <table>
+          
+          <thead>
+            <tr>
+              {this.props.headerOrder.map(this.renderHeader)}
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data.map(this.renderRow)}
+          </tbody>
+        </table>
+      </div>
     );
   }
 
-  private renderHeader = (value: number) => {
+  public componentDidMount(): void {
+    window.addEventListener('mouseup', this.props.onMouseUp)
+    window.addEventListener('mousemove', this.onMouseMove)
+  }
+
+  public componentWillUnmount(): void {
+    window.removeEventListener('mouseup', this.props.onMouseUp)
+    window.removeEventListener('mousemove', this.onMouseMove)
+  }
+
+  private onMouseDown(event: React.MouseEvent, col: number) {
+    this.props.onMouseDown(event.clientX, col);
+  }
+
+  private onMouseMove = (event: MouseEvent) => {
+    this.props.onMouseMove(event.clientX)
+  }
+
+  private renderHeader = (col: number) => {
+    const isSlidingColumn = this.props.slidingColumn === col;
     return (
-      <th id={`${0}-${value}`} key={`${0}-${value}`}
-          onMouseDown={() => this.props.onMouseDown(value)}>
-        {this.props.header[value]}
+      <th id={`${0}-${col}`} key={`${0}-${col}`}
+          className={isSlidingColumn ? 'header-selected ' : ''}
+          onMouseDown={(event) => this.onMouseDown(event, col)}>
+        {this.props.slidingColumn === col? '' :this.props.header[col]}
       </th>);
   }
 
   private renderRow = (row_data: string[], row: number) => {
     return (
       <tr id={`${row}`} key={`${row}`}>
-        {this.props.headerOrder.map(value => {
-          const id= `${row}-${value}`
+        {this.props.headerOrder.map(col => {
+          const id= `${row}-${col}`
+          const isSlidingColumn = this.props.slidingColumn === col;
+          const isBottomRow = row === 3;
+          const className = (() => {
+            if(isBottomRow && isSlidingColumn) {
+              return 'bottom-selected'
+            } else if (isSlidingColumn) {
+              return 'td-selected'
+            } else {
+              return ''
+            }
+          })();
           return (
-            <td id={`${row}-${value}`} key={`${row}-${value}`}>
-              {row_data[value]}
+            <td id={`${row}-${col}`} key={`${row}-${col}`}
+              className={className}>
+              {this.props.slidingColumn === col? '' : row_data[col]}
             </td>);
         })}
       </tr>);
   }
 
-  public componentDidMount(): void {
-    window.addEventListener('mouseup', this.props.onMouseUp)
+  private getStyle =(row: number, column: number) => {
+    if('hidden') {
+      return
+    } else if('highlighetd'){
+      return
+    } else {
+      return
+    }
   }
 
-  public componentWillUnmount(): void {
-    window.removeEventListener('mouseup', this.props.onMouseUp)
+
+}
+
+interface SlidingColProperties {
+  leftValue: number;
+  show: boolean;
+}
+
+class SlidingColumn extends React.Component<SlidingColProperties> {
+  constructor(props: SlidingColProperties) {
+    super(props);
+  }
+
+  public render(): JSX.Element {
+    if(!this.props.show) {
+      return <div style={{display: 'none'}}/>
+    }
+    return (
+      <div className='sliding-column-container' style={{left: this.props.leftValue}}>
+        <table className='sliding-column-table '>
+          <thead>
+            <tr>
+              <th>
+              {'header'}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr><td>Some data</td></tr>
+            <tr><td> Some data</td></tr>
+            <tr><td> Some data</td></tr>
+            <tr><td> Some data</td></tr>
+          </tbody>
+        </table>
+      </div>);
   }
 }
 
